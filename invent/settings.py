673912ddd9a3b4019f7407bac9eb024d6563a825
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2016-09-13 12:24:32 +0530

    updated .gitignore and added mypress app to settings.py

--- invent/settings.py
+++ invent/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mypress.apps.MypressConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
