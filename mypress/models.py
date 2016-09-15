1fa4d66e237e87cd5f80d630676a2b3383a88993
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2016-09-15 22:40:17 +0530

    Added the FileField() in post() model to allow user to add image for post urls.py added urls for blogs blog/post_id views.py specified new views for blog posts still lot of work to be done

--- mypress/models.py
+++ mypress/models.py
@@ -5,7 +5,8 @@ class post(models.Model):
     post_title = models.CharField(max_length=200)
     post_author = models.CharField(max_length=100)
     pub_date = models.DateTimeField()
-    post_article = models.TextField(max_length=3500)
+    post_article = models.TextField(max_length=10000)
+    post_img = models.FileField()
 
     def __str__(self):
         return "" + self.post_title + " | " + self.post_author
--- mypress/views.py
+++ mypress/views.py
@@ -1,11 +1,25 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 from .models import post
 
 def index(request):
     post_details = post.objects.filter(id='1')
+    all_posts = post.objects.all()
     context = {
-        'post' : post_details
+        'post' : post_details,
+        'allposts' : all_posts
     }
-    template = loader.get_template('blog/post.html');
+    template = loader.get_template('blog/footer.html');
+    return HttpResponse(template.render(context,request))
+
+
+def posts(request,post_id):
+    post_details = post.objects.get(pk=post_id)
+    all_posts = post.objects.all()
+    context = {
+        'post' : post_details,
+        'allposts' : all_posts,
+    }
+    template = loader.get_template('blog/footer.html');
     return HttpResponse(template.render(context,request))
